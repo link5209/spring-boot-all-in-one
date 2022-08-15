839f15a70d74f959426614fcef939af5dc75bd5f
huangyi <huangyi@guanghe.tv>
2022-08-15 11:24:54 +0800

    built all things for Spring Security.

---
+++ src/main/java/com/springboot/allinone/entity/Role.java
@@ -0,0 +1,35 @@
+package com.springboot.allinone.entity;
+
+import com.springboot.allinone.model.ERole;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
