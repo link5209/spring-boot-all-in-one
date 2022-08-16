697463138e076c35f71950e778011662f6b5d50e
huangyi <huangyi@guanghe.tv>
2022-08-16 08:51:39 +0800

    .anyRequest().permitAll

--- src/main/java/com/springboot/allinone/security/WebSecurityConfig.java
+++ src/main/java/com/springboot/allinone/security/WebSecurityConfig.java
@@ -52,7 +52,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().permitAll();
+//                .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
