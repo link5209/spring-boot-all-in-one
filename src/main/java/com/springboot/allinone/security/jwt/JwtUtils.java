1312644e9b24e9f7b7da278a3956102ec27c923c
huangyi <huangyi@guanghe.tv>
2022-08-22 12:02:57 +0800

    add druid

--- src/main/java/com/springboot/allinone/security/jwt/JwtUtils.java
+++ src/main/java/com/springboot/allinone/security/jwt/JwtUtils.java
@@ -14,10 +14,10 @@ import java.util.Date;
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${my.app.jwtSecret}")
+    @Value("${jwt.secret}")
     private String jwtSecret;
 
-    @Value("${my.app.jwtExpirationMs}")
+    @Value("${jwt.expiration}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
